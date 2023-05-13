2e6b6d9ae86b6e9f7c8da68c33e9c4512fb51199
reimar <me@reimar.org>
2023-05-13 10:12:19 +0200

    Add stars to mark days

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -5,14 +5,16 @@ namespace App\Entity;
 use App\Repository\CommentRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
 class Comment
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(type: 'uuid', unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private $id = null;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $message = null;
@@ -20,7 +22,7 @@ class Comment
     #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $date = null;
 
-    public function getId(): ?int
+    public function getId(): ?Uuid
     {
         return $this->id;
     }
